861e5f157031e95d1597aca0a1cebf891caa6328
Paulo Henrique Dias <34211685+PauloHenriqueDias@users.noreply.github.com>
2021-11-25 21:49:21 -0300

    Update script.js

--- site_gamer/js/script.js
+++ site_gamer/js/script.js
@@ -153,7 +153,7 @@ function gerar_json(form){
     var dados= {nome, cpf, telefone_res, telefone_cel, cep, endereco, numero, bairro, cidade, estado, ibge};
 
 
-    var formularioValido = validarNome() && confereCPF();
+    var formularioValido = validarNome() && confereCPF() && telefone_validation();
     if( formularioValido){
         document.write("<h2> Retorno em JSON </h2>");
         document.write(JSON.stringify(dados, null, '<br>'));
@@ -162,6 +162,53 @@ function gerar_json(form){
         document.form.focus();
     }   
 
+}
+//*********************** validar Telefone **********************************
+function telefone_validation() {
+    telefone = document.getElementById("telefone_res" ).value;
+    celular = document.getElementById("telefone_cel").value;
+    //retira todos os caracteres menos os numeros
+    telefone = telefone.replace(/\D/g, '');
+    celular = celular.replace(/\D/g, '');
+
+    //verifica se tem a qtde de numero correto
+    if (!(telefone.length >= 10 && telefone.length <= 11)) return false;
+    if(!(celular.length >= 10 && celular.length <= 11)) return false;
+
+    //Se tiver 11 caracteres, verificar se começa com 9 o celular
+    if (telefone.length == 11 && parseInt(telefone.substring(2, 3)) != 9) return false;
+    if (celular.length == 11 && parseInt(celular.substring(2, 3)) != 9) return false;
+
+    //verifica se não é nenhum numero digitado errado (propositalmente)
+    for (var n = 0; n < 10; n++) {
+        //um for de 0 a 9.
+        //estou utilizando o metodo Array(q+1).join(n) onde "q" é a quantidade e n é o
+        //caractere a ser repetido
+        if (telefone == new Array(11).join(n) || telefone == new Array(12).join(n)) return false;
+        if (celular == new Array(11).join(n) || celular == new Array(12).join(n)) return false;
+        
+    }
+    //DDDs validos
+    var codigosDDD = [11, 12, 13, 14, 15, 16, 17, 18, 19,
+        21, 22, 24, 27, 28, 31, 32, 33, 34,
+        35, 37, 38, 41, 42, 43, 44, 45, 46,
+        47, 48, 49, 51, 53, 54, 55, 61, 62,
+        64, 63, 65, 66, 67, 68, 69, 71, 73,
+        74, 75, 77, 79, 81, 82, 83, 84, 85,
+        86, 87, 88, 89, 91, 92, 93, 94, 95,
+        96, 97, 98, 99];
+    //verifica se o DDD é valido (sim, da pra verificar rsrsrs)
+    if (codigosDDD.indexOf(parseInt(telefone.substring(0, 2))) == -1) return false;
+    if (codigosDDD.indexOf(parseInt(celular.substring(0, 2))) == -1) return false;
+
+    
+    if (new Date().getFullYear() < 2017) return true;
+    if (telefone.length == 10 && [2, 3, 4, 5, 7].indexOf(parseInt(telefone.substring(2, 3))) == -1) return false;
+        alert("telefone valido")
+    if (celular.length == 10 && [2, 3, 4, 5, 7].indexOf(parseInt(celular.substring(2, 3))) == -1) return false;
+        alert("celular valido")
+    //se passar por todas as validações acima, então está tudo certo
+    return true;
 }
 
 //mascaras
